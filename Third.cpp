3bf3beefc9f7b6608088057ec666bf25bdcf34b5
krisik139 <sirenkomaksym95@gmail.com>
2026-07-05 01:05:34 +0300

    фіфіавіаіфваіфва

---
+++ Third.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    system("chcp 1251>null");
+    int arr[10];
+
+    for (int i = 0; i < 10; i++) {
+        arr[i] = 99 - (i * 3);
+    }
+
+    cout << "Спадаючий масив: ";
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
