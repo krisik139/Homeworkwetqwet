eb4b337f20f72c7d34a34781203a2a77b4c4c4a3
krisik139 <sirenkomaksym95@gmail.com>
2026-07-05 01:10:19 +0300

    cimmit

---
+++ Fourth.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    system("chcp 1251>null");
+
+    int bigArr[10] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+    int part1[5];
+    int part2[5];
+
+    for (int i = 0; i < 5; i++) {
+        part1[i] = bigArr[i];
+        part2[i] = bigArr[i + 5];
+    }
+
+    cout << "first масив 5 елементів: ";
+    for (int i = 0; i < 5; i++) cout << part1[i] << " ";
+    cout << endl;
+
+    cout << "secons масив 5 елементів: ";
+    for (int i = 0; i < 5; i++) cout << part2[i] << " ";
+    cout << endl;
+
+    return 0;
+}
